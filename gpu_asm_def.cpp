63f5656d44ae71ec6055c580af914cb91b741385
Rák Ádám <rakadam@rakadam.dyndns.org>
2011-04-28 01:18:07 +0200

    parsing works with error checking, without semantic actions

--- gpu_asm_def.cpp
+++ gpu_asm_def.cpp
@@ -5,6 +5,7 @@
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/spirit/repository/include/qi_confix.hpp>
 
+
 #include "gpu_asm_def.hpp"
 
 using namespace boost::spirit::qi;
@@ -23,8 +24,60 @@ using namespace std;
 // 	asm_definition(std::string text); //parse definitions from text
 // };
 
+
+struct printer
+{
+		bool detailed;
+		
+    typedef boost::spirit::utf8_string string;
+
+		printer(bool detailed = false) : detailed(detailed) {}
+		
+    void element(string const& tag, string const& value, int depth) const
+    {
+			for (int i = 0; i < (8+depth*4); ++i) // indent to depth
+			{
+				std::cout << ' ';
+			}
+				
+			if (detailed)
+			{
+				
+				std::cout << "tag: " << tag;
+				
+				if (value != "")
+				{
+					std::cout << ", value: \"" << value << "\"";
+				}
+				
+				std::cout << std::endl;
+			}
+			else
+			{
+				if (value != "")
+				{
+					cout << value << endl;
+				}
+				else
+				{
+					cout << tag << endl;
+				}
+			}
+    }
+};
+
+void print_info(boost::spirit::info const& what, bool detailed = false)
+{
+    using boost::spirit::basic_info_walker;
+
+		cout << endl;
+    printer pr(detailed);
+    basic_info_walker<printer> walker(pr, what.tag, 0);
+    boost::apply_visitor(walker, what.value);
+}
+
 namespace gpu_asm{
-	
+
 struct assign_str
 {
 	std::string& str;
@@ -36,6 +89,14 @@ struct assign_str
 	void operator()(const vector<char>& s, qi::unused_type, qi::unused_type) const
 	{
 		str = std::string(s.begin(), s.end());
+	}
+};
+
+struct print_str
+{
+	void operator()(const vector<char>& s, qi::unused_type, qi::unused_type) const
+	{
+		cout << std::string(s.begin(), s.end()) << endl;
 	}
 };
 
@@ -65,31 +126,97 @@ asm_definition::asm_definition(std::string text)
 	bound cur_bound;
 	std::map<std::string, std::set<enum_val> > enums;
 	
-	auto name = lexeme[*(alnum)];
+	auto name = lexeme[*(alnum >> *lit("_"))];
 	
-	auto header = "architecture" >> name[assign_str(arch_techname)] >> name[assign_str(arch_codename)] >> ';';
+	auto header = "architecture" > name[assign_str(arch_techname)] > name[assign_str(arch_codename)] > ';';
 	auto bbound_p = ('(' >> int_ >> ')') | ('(' >> int_ >> ':' >> int_ >> ')'); 
 	auto bound_p = (int_ >> ':' >> int_) | int_; 
-	auto size_p = ('(' >> int_ >> ')');
+	auto size_p = ('(' > int_ > ')');
 	
-	auto enum_elem = name >> bound_p >> ';';
-	auto enum_def = "enum" >> size_p >> name >> ':' >> *enum_elem >> "end" >> "enum" >> ';';
-	auto field = "field" >> name >> bbound_p >> name >> ';';
-	auto microcode_def = "microcode" >> name >> bbound_p >> ':' >> *(enum_def | field) >> "end" >> "microcode" >> ';';
-/*	auto tuple_def = ;
-	auto microcode_use = ;
-	auto constraints_def = ;
-	auto constraint_def = ;*/
+	auto debug = lexeme[(*char_)[print_str()]];
+	auto enum_elem = !lit("end") > name > -bound_p > ';';
+	auto enum_def = "enum" > size_p > name > ':' > *enum_elem > "end" > "enum" > ';';
+	auto field = "field" > name > bbound_p > name >> ';';
+	auto microcode_def = "microcode" > name >  bbound_p > ':'  > *(enum_def | field)  > "end" > "microcode" > ';';
+	auto microcode_use = "microcode" > name >> ';';
+	auto constraint_def = !lit("end") > name > '.' > name > "==" > name > ';';
+	auto constraints_def = "constraints" >> lit(':') > *constraint_def > "end" > "constraints" > ';';
+	auto tuple_def = "tuple" > name >  bbound_p > ':'  > *microcode_use > -constraints_def > "end" > "tuple" > ';';
 	
 	auto begin = text.begin();
 	auto end = text.end();
-	phrase_parse(begin, end, header >> microcode_def, space);
 	
+	int linenum = 0;
 	
-	cout << arch_techname << "-" << arch_codename << endl;
+	for (int i = 0; i < text.length(); i++)
+		if (text[i] == '\n')
+			linenum++;
 	
-	
- 	cout << std::string(begin, end) << endl;
+	try{
+		phrase_parse(begin, end, eps > header > *(microcode_def | tuple_def) > "end" > ';' > eoi, space);
+	}
+	catch (expectation_failure<decltype(begin)> const& x)
+	{
+		std::cout << "expected: "; print_info(x.what_);
+		std::string got(x.first, x.last);
+		
+		int gotlinenum = 0;
+		
+		for (int i = 0; i < got.length(); i++)
+			if (got[i] == '\n')
+				gotlinenum++;
+
+		int gotsize = got.size();
+		
+		if (got.find("\n") != std::string::npos)
+		{
+			got.resize(got.find("\n"));
+		}
+		
+		std::cout << "got: \"" << got << "\" at line: #" << linenum - gotlinenum +1 << std::endl;
+		
+		int pos = 0;
+		int cpos = int(text.length()) - gotsize;
+		
+		for (int i = int(text.length()) - gotsize; i >= 0; i--)
+		{
+			if (text[i] == '\n')
+			{
+				pos = i+1;
+				break;
+			}
+		}
+		
+		int pos2 = text.length();
+		
+		for (int i = pos; i < text.length(); i++)
+		{
+			if (text[i] == '\n')
+			{
+				pos2 = i;
+				break;
+			}
+		}
+		
+		std::string line(text.begin()+pos, text.begin()+pos2);
+		
+		for (int i = 0; i < int(line.length()); i++)
+		{
+			if (line[i] == '\t')
+			{
+				line[i] = ' ';
+			} 
+		}
+		
+		std::cout << line << endl;
+		
+		for (int i = 0; i < cpos-pos; i++)
+		{
+			cout << " ";
+		}
+		
+		cout << "^" << endl;
+	}
 }
 
 
